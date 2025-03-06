03a6eb79dea16423d74e02b751f4fc9320082514
margo <margo2356@outlook.de>
2025-03-06 18:43:07 +0100

    projekt update

--- src/main/java/lesson_44/ClassWork44/Main1.java
+++ src/main/java/lesson_44/ClassWork44/Main1.java
@@ -4,7 +4,7 @@ public class Main1 {
     public static void main(String[] args) {
         Museum<Artwork> museum = new Museum<>();
         museum.addExhibit(new Painting("Мона Лиза", "Леонардо да Винчи"));
-        museum.addExhibit(new Sculpture("Давид", "Микеланджело"));
+        museum.addExhibit(new Sculpture("Давид", "Гранит"));
 
         System.out.println("Экспонаты музея:");
         museum.displayExhibits();
